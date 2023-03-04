c0f3b6731bbdda275921ddd764b198c82382e708
Vinicius Frias <vmofrias@gmail.com>
2023-03-04 17:44:26 -0300

    Criação de um DTO para o model Restaurante utilizando o id para utilizar em operações como de update ou delete

---
+++ src/main/java/com/ldsk/restaurant/dto/RestauranteIdDto.java
@@ -0,0 +1,27 @@
+package com.ldsk.restaurant.dto;
+
+import java.math.BigDecimal;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class RestauranteIdDto {
+	
+	private Long id;
+
+	@JsonProperty(value = "email_owner")
+	private String emailOwner;
+	
+	private String nome;
+	
+	@JsonProperty(value = "taxa_frete")
+	private BigDecimal taxaFrete;
+
+	@JsonProperty(value = "nome_cozinha")
+	private String nomeCozinha;
+	
+}
