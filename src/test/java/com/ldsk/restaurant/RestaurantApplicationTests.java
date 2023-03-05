1dcf5c77e20dc3d01639e94421e931961b5ed952
Vinicius Frias <vmofrias@gmail.com>
2023-03-05 19:15:23 -0300

    Implementação de testes básicos

---
+++ src/test/java/com/ldsk/restaurant/RestaurantApplicationTests.java
@@ -0,0 +1,34 @@
+package com.ldsk.restaurant;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+//@SpringBootTest
+class RestaurantApplicationTests {
+
+	Calculator underTest = new Calculator();
+	
+	@Test
+	void itShouldAddTwoNumbers() {
+		
+		//given
+		int numberOne = 20;
+		int numberTwo = 30;
+		
+		//when
+		int result = underTest.add(numberOne, numberTwo);
+		
+		//then
+		assertThat(result).isEqualTo(50);
+	}
+
+	class Calculator {
+		
+		int add(int a, int b) {
+			
+			return a+b;
+		}
+	}
+	
+}
