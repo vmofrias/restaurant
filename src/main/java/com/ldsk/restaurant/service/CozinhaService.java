71d9ca0ca5ae34cb6b8a9afcece968f709a2bc64
Vinicius Frias <vmofrias@gmail.com>
2023-05-20 10:38:33 -0300

    Implementação dos métodos de adicionar, atualizar e deletar cozinha

--- src/main/java/com/ldsk/restaurant/service/CozinhaService.java
+++ src/main/java/com/ldsk/restaurant/service/CozinhaService.java
@@ -12,10 +12,14 @@ import com.ldsk.restaurant.exception.CozinhaException;
 import com.ldsk.restaurant.model.Cozinha;
 import com.ldsk.restaurant.repository.CozinhaRepository;
 
+import jakarta.persistence.EntityExistsException;
+
 @Service
 public class CozinhaService {
 	
 	private static final String ERRO_REQUISICAO = "Erro na requisição";
+	private static final String ERRO_SERVIDOR = "Erro no processamento da requisição";
+	private static final String COZINHA_DELETED_MESSAGE = "Cozinha com id %d deletada com sucesso!";
 	
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
@@ -23,6 +27,11 @@ public class CozinhaService {
 	public List<Cozinha> getCozinhas() {
 		
 		return cozinhaRepository.findAll();
+	}
+	
+	public Cozinha getCozinhaByNome(Cozinha cozinha) {
+		
+		return getCozinhaByNome(cozinha.getNome());
 	}
 
 	public Cozinha getCozinhaByNome(String nome) {
@@ -47,5 +56,78 @@ public class CozinhaService {
 			throw new CozinhaException(e.getLocalizedMessage(), ERRO_REQUISICAO);
 		}
 	}
+	
+	public Cozinha addCozinha(Cozinha cozinha) {
+		
+		try {
+			
+			if(cozinha.getId() == null) cozinha.setId(0L); 
+				
+			Optional<Cozinha> cozinhaOptional = cozinhaRepository.findById(cozinha.getId());
+			
+			if(cozinhaOptional.isEmpty()) {
+				
+				return cozinhaRepository.save(cozinha);
+			} else {
+				
+				throw new EntityExistsException();
+			}
+		} catch (EntityExistsException e) {
+			
+			throw new CozinhaException(String.format("Não foi possível adicionar esta cozinha pois o id %d já existe.", cozinha.getId()),
+					ERRO_REQUISICAO, HttpStatus.BAD_REQUEST);
+		} catch (Exception e) {
+			
+			throw new CozinhaException(e.getLocalizedMessage(), ERRO_SERVIDOR, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	
+	public Cozinha updateCozinha(Cozinha cozinha) {
+		
+		try {
+			
+			Optional<Cozinha> cozinhaOptional = cozinhaRepository.findById(cozinha.getId());
+			
+			if(cozinhaOptional.isPresent()) {
+				
+				return cozinhaRepository.save(cozinha);
+			} else {
+				
+				throw new NoSuchElementException();
+			}
+		} catch (NoSuchElementException e) {
+			
+			throw new CozinhaException(String.format("Não existe uma cozinha com id %d para ser atualizada.", cozinha.getId()),
+					ERRO_REQUISICAO, HttpStatus.BAD_REQUEST);
+		} catch (Exception e) {
+
+			throw new CozinhaException(e.getLocalizedMessage(), ERRO_SERVIDOR, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}	
+	
+	public String deleteCozinhaById(Cozinha cozinha) {
+		
+		try {
+			
+			Optional<Cozinha> cozinhaOptional = cozinhaRepository.findById(cozinha.getId());
+			
+			if(cozinhaOptional.isPresent()) {
+				
+				cozinhaRepository.deleteById(cozinha.getId());
+				
+				return String.format(COZINHA_DELETED_MESSAGE, cozinha.getId());
+			} else {
+				
+				throw new NoSuchElementException();
+			}
+		} catch (NoSuchElementException e) {
+			
+			throw new CozinhaException(String.format("Não existe uma cozinha com id %d para ser deletada.", cozinha.getId()),
+					ERRO_REQUISICAO, HttpStatus.BAD_REQUEST);
+		} catch (Exception e) {
+			
+			throw new CozinhaException(e.getLocalizedMessage(), ERRO_SERVIDOR, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}	
 	
 }
