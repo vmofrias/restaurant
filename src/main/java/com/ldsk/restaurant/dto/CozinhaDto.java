322e6a97bb892de06911814a6cc3a78b539471aa
Vinicius Frias <vmofrias@gmail.com>
2023-05-20 10:36:58 -0300

    Implementação dos DTOs para cozinha

---
+++ src/main/java/com/ldsk/restaurant/dto/CozinhaDto.java
@@ -0,0 +1,16 @@
+package com.ldsk.restaurant.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CozinhaDto {
+
+	private String nome;
+	
+}
---
+++ src/main/java/com/ldsk/restaurant/dto/CozinhaIdDto.java
@@ -0,0 +1,18 @@
+package com.ldsk.restaurant.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CozinhaIdDto {
+	
+	private Long id;
+
+	private String nome;
+	
+}
