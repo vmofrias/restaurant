997b27a29d3cb53d7fd6e1daf8d72e4e9f744223
Vinicius Frias <vmofrias@gmail.com>
2023-05-21 12:02:15 -0300

    Inclusão da anotação @DataJpaTest para testar componentes de acesso a dados

--- src/test/java/com/ldsk/restaurant/repository/CozinhaRepositoryTest.java
+++ src/test/java/com/ldsk/restaurant/repository/CozinhaRepositoryTest.java
@@ -6,9 +6,11 @@ import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import com.ldsk.restaurant.model.Cozinha;
 
+@DataJpaTest
 class CozinhaRepositoryTest {
 
 	@Autowired
