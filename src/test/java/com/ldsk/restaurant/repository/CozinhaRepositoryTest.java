3564d2d200bee09fada19ea4f1602c690e2b3798
Vinicius Frias <vmofrias@gmail.com>
2023-05-21 17:21:25 -0300

    Implementado teste itShouldNotFindByNome()

--- src/test/java/com/ldsk/restaurant/repository/CozinhaRepositoryTest.java
+++ src/test/java/com/ldsk/restaurant/repository/CozinhaRepositoryTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Optional;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -15,6 +16,11 @@ class CozinhaRepositoryTest {
 
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
+	
+	@AfterEach
+	void tearDown() {
+		cozinhaRepository.deleteAll();
+	}
 	
 	@Test
 	void itShouldFindByNome() {
@@ -31,7 +37,21 @@ class CozinhaRepositoryTest {
 		
 		// then
 		assertThat(cozinhaOptional.get().getNome()).isEqualTo("Africana");
+	}
+	
+	@Test
+	void itShouldNotFindByNome() {
 		
+		// given
+		Cozinha cozinha = Cozinha.builder()
+			.nome("Africana")
+			.build();
+		
+		// when
+		Optional<Cozinha> cozinhaOptional = cozinhaRepository.findByNome(cozinha.getNome());
+		
+		// then
+		assertThat(cozinhaOptional.isEmpty()).isTrue();
 	}
 	
 }
