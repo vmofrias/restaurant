58f90c69f17739a86faa0489d9f9ba3357c3489c
Vinicius Frias <vmofrias@gmail.com>
2023-05-21 12:02:59 -0300

    Adicionado constraint UNIQUE no atributo nome da Cozinha

--- src/main/java/com/ldsk/restaurant/model/Cozinha.java
+++ src/main/java/com/ldsk/restaurant/model/Cozinha.java
@@ -29,7 +29,7 @@ public class Cozinha {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@Column(nullable = false)
+	@Column(nullable = false, unique = true)
 	private String nome;
 	
 	@JsonIgnore
