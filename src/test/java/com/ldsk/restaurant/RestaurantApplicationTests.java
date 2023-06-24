240884d256aff09a2283206640a7e920b40eb723
Vinicius Frias <vmofrias@gmail.com>
2023-06-24 14:26:52 -0300

    Aplicado teste de contexto da aplicação

--- src/test/java/com/ldsk/restaurant/RestaurantApplicationTests.java
+++ src/test/java/com/ldsk/restaurant/RestaurantApplicationTests.java
@@ -1,34 +1,16 @@
 package com.ldsk.restaurant;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-//@SpringBootTest
+@SpringBootTest
+@ActiveProfiles("testing")
 class RestaurantApplicationTests {
 
-	Calculator underTest = new Calculator();
-	
 	@Test
-	void itShouldAddTwoNumbers() {
+	void contextLoads() {
 		
-		//given
-		int numberOne = 20;
-		int numberTwo = 30;
-		
-		//when
-		int result = underTest.add(numberOne, numberTwo);
-		
-		//then
-		assertThat(result).isEqualTo(50);
-	}
-
-	class Calculator {
-		
-		int add(int a, int b) {
-			
-			return a+b;
-		}
 	}
 	
 }
