688c3836f3984e4f03737ec54aeccf3cadeda0f0
Vinicius Frias <vmofrias@gmail.com>
2023-06-25 15:24:24 -0300

    Implementado testes básicos para os métodos do CozinhaService

--- src/test/java/com/ldsk/restaurant/service/CozinhaServiceTests.java
+++ src/test/java/com/ldsk/restaurant/service/CozinhaServiceTests.java
@@ -1,11 +1,15 @@
 package com.ldsk.restaurant.service;
 
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -25,9 +29,16 @@ class CozinhaServiceTests {
 	@InjectMocks
 	private CozinhaService cozinhaService;
 	
+	@AfterEach
+	void tearDown() {
+		
+		cozinhaRepository.deleteAll();
+	}
+	
 	@Test
 	void itShouldGetAllCozinhas() {
 		
+		// Arrange
 		List<Cozinha> cozinhas = Arrays.asList(
 				Cozinha.builder().nome("cozinha1").build(),
 				Cozinha.builder().nome("cozinha2").build());
@@ -35,24 +46,92 @@ class CozinhaServiceTests {
 		
 		when(cozinhaRepository.findAll()).thenReturn(cozinhas);
 		
+		// Act
 		List<Cozinha> cozinhaList = cozinhaService.getCozinhas();
 	
+		// Assert
 		Assertions.assertThat(cozinhaList)
 			.isNotNull()
 			.hasSize(2);
+		
+		verify(cozinhaRepository, times(1)).findAll();
 	}
 	
 	@Test
-	void itShouldCreateACozinha() {
+	void itShouldGetCozinhaByNome() {
 		
+		// Arrange
 		Cozinha cozinha = Cozinha.builder().nome("cozinha").build();
 		
+		when(cozinhaRepository.findByNome(Mockito.anyString())).thenReturn(Optional.of(cozinha));
+		
+		// Act
+		Cozinha cozinhaReturn = cozinhaService.getCozinhaByNome(cozinha);
+	
+		// Assert
+		Assertions.assertThat(cozinhaReturn).isNotNull();
+		Assertions.assertThat(cozinhaReturn.getNome()).isEqualTo(cozinha.getNome());
+		
+		verify(cozinhaRepository, times(1)).findByNome(Mockito.anyString());
+	}
+	
+	@Test
+	void itShouldAddCozinha() {
+		
+		// Arrange
+		Cozinha cozinha = Cozinha.builder().id(0L).nome("cozinha").build();
+		
+		when(cozinhaRepository.findById(cozinha.getId())).thenReturn(Optional.empty());
 		when(cozinhaRepository.save(Mockito.any(Cozinha.class))).thenReturn(cozinha);
 		
+		// Act
 		Cozinha savedCozinha = cozinhaService.addCozinha(cozinha);
 	
+		// Assert
 		Assertions.assertThat(savedCozinha).isNotNull();
 		Assertions.assertThat(savedCozinha.getNome()).isEqualTo("cozinha");
+		
+		verify(cozinhaRepository, times(1)).findById(cozinha.getId());
+		verify(cozinhaRepository, times(1)).save(Mockito.any(Cozinha.class));
+	}
+	
+	@Test
+	void itShouldUpdateCozinha() {
+		
+		// Arrange
+		Cozinha cozinha = Cozinha.builder().id(1L).nome("cozinha").build();
+		
+		when(cozinhaRepository.findById(cozinha.getId())).thenReturn(Optional.of(cozinha));
+		when(cozinhaRepository.save(Mockito.any(Cozinha.class))).thenReturn(cozinha);
+		
+		// Act
+		Cozinha updatedCozinha = cozinhaService.updateCozinha(cozinha);
+	
+		// Assert
+		Assertions.assertThat(updatedCozinha)
+			.isNotNull()
+			.isEqualTo(cozinha);
+		
+		verify(cozinhaRepository, times(1)).findById(cozinha.getId());
+        verify(cozinhaRepository, times(1)).save(Mockito.any(Cozinha.class));
+	}
+	
+	@Test
+	void itShouldDeleteCozinha() {
+		
+		// Arrange
+		Cozinha cozinha = Cozinha.builder().id(1L).nome("cozinha").build();
+		
+		when(cozinhaRepository.findById(cozinha.getId())).thenReturn(Optional.of(cozinha));
+		
+		// Act
+        String result = cozinhaService.deleteCozinhaById(cozinha);
+
+        // Assert
+        Assertions.assertThat(result).isNotEmpty();
+        
+        verify(cozinhaRepository, times(1)).findById(cozinha.getId());
+        verify(cozinhaRepository, times(1)).deleteById(cozinha.getId());
 	}
 	
 }
