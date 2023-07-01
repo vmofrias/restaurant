7515bc6181afb5c1f48db10eb93cdaf812fba5ae
Vinicius Frias <vmofrias@gmail.com>
2023-07-01 17:10:38 -0300

    Ajuste no seeding do banco de dados

--- src/main/resources/db/datatest/afterMigrate.sql
+++ src/main/resources/db/datatest/afterMigrate.sql
@@ -1,15 +1,15 @@
 DELETE FROM restaurant_schema.restaurante;
 DELETE FROM restaurant_schema.cozinha;
 
-INSERT INTO restaurant_schema.cozinha VALUES (1, 'Brasileira');
-INSERT INTO restaurant_schema.cozinha VALUES (2, 'Japonesa');
-INSERT INTO restaurant_schema.cozinha VALUES (3, 'Italiana');
-INSERT INTO restaurant_schema.cozinha VALUES (4, 'Coreana');
-INSERT INTO restaurant_schema.cozinha VALUES (5, 'Mexicana');
-INSERT INTO restaurant_schema.cozinha VALUES (6, 'Francesa');
-INSERT INTO restaurant_schema.cozinha VALUES (7, 'Chinesa');
-INSERT INTO restaurant_schema.cozinha VALUES (8, 'Norte Americana');
+INSERT INTO restaurant_schema.cozinha(nome) VALUES ('Brasileira');
+INSERT INTO restaurant_schema.cozinha(nome) VALUES ('Japonesa');
+INSERT INTO restaurant_schema.cozinha(nome) VALUES ('Italiana');
+INSERT INTO restaurant_schema.cozinha(nome) VALUES ('Coreana');
+INSERT INTO restaurant_schema.cozinha(nome) VALUES ('Mexicana');
+INSERT INTO restaurant_schema.cozinha(nome) VALUES ('Francesa');
+INSERT INTO restaurant_schema.cozinha(nome) VALUES ('Chinesa');
+INSERT INTO restaurant_schema.cozinha(nome) VALUES ('Norte Americana');
 
-INSERT INTO restaurant_schema.restaurante  VALUES (1, 'vmofrias@gmail.com', 'Restaurante do Vinicius', 10.0, 1);
-INSERT INTO restaurant_schema.restaurante  VALUES (2, 'ldskmain@gmail.com', 'Restaurante do LDSK', 15.0, 2);
-INSERT INTO restaurant_schema.restaurante  VALUES (3, 'bia@gmail.com', 'Restaurante da Bibia', 5.0, 3);
+INSERT INTO restaurant_schema.restaurante(email_owner, nome, taxa_frete, cozinha_id)  VALUES ('vmofrias@gmail.com', 'Restaurante do Vinicius', 10.0, 1);
+INSERT INTO restaurant_schema.restaurante(email_owner, nome, taxa_frete, cozinha_id)  VALUES ('ldskmain@gmail.com', 'Restaurante do LDSK', 15.0, 2);
+INSERT INTO restaurant_schema.restaurante(email_owner, nome, taxa_frete, cozinha_id)  VALUES ('bia@gmail.com', 'Restaurante da Bibia', 5.0, 3);
