b8c5bbf00b48d6f96c3d6980a653d66327b8efaa
Vinicius Frias <vmofrias@gmail.com>
2023-07-03 19:54:43 -0300

    Alteração do nome da classe UsuarioMapper para AuthMapper

--- src/main/java/com/ldsk/restaurant/controller/AuthController.java
+++ src/main/java/com/ldsk/restaurant/controller/AuthController.java
@@ -3,22 +3,17 @@ package com.ldsk.restaurant.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ldsk.restaurant.bo.AuthBo;
-import com.ldsk.restaurant.config.JWTProvider;
 import com.ldsk.restaurant.dto.AuthLoginRequestDto;
 import com.ldsk.restaurant.dto.AuthLoginResponseDto;
 import com.ldsk.restaurant.dto.AuthRegisterRequestDto;
 import com.ldsk.restaurant.dto.AuthRegisterResponseDto;
-import com.ldsk.restaurant.mapper.UsuarioMapper;
+import com.ldsk.restaurant.mapper.AuthMapper;
 
 import jakarta.validation.Valid;
 
@@ -27,22 +22,16 @@ import jakarta.validation.Valid;
 public class AuthController {
 	
 	@Autowired
-	private AuthenticationManager authenticationManager;
-	
-	@Autowired
 	private AuthBo authBo;
 	
 	@Autowired
-	private UsuarioMapper usuarioMapper;
-	
-	@Autowired
-	private JWTProvider jwtProvider;
+	private AuthMapper authMapper;
 	
 	@PostMapping(value = "/register")
 	public ResponseEntity<AuthRegisterResponseDto> registerUser(@RequestBody @Valid AuthRegisterRequestDto authRegisterRequestDto) {
 		
-		AuthRegisterResponseDto authRegisterResponseDto = usuarioMapper.toAuthRegisterResponseDto(
-				authBo.registerUsuario(usuarioMapper.toUsuarioWithEncryptedPassword(authRegisterRequestDto)));
+		AuthRegisterResponseDto authRegisterResponseDto = authMapper.toAuthRegisterResponseDto(
+				authBo.registerUsuario(authMapper.toUsuarioWithEncryptedPassword(authRegisterRequestDto)));
 		
 		return ResponseEntity.status(HttpStatus.CREATED).body(authRegisterResponseDto);
 	}
@@ -50,14 +39,10 @@ public class AuthController {
 	@PostMapping(value = "/login")
 	public ResponseEntity<AuthLoginResponseDto> loginUser(@RequestBody @Valid AuthLoginRequestDto authLoginRequestDto) {
 		
-		Authentication authentication = authenticationManager.authenticate(
-				new UsernamePasswordAuthenticationToken(authLoginRequestDto.getUsername(), authLoginRequestDto.getPassword()));
+		AuthLoginResponseDto authLoginResponseDto = authMapper.toAuthLoginResponseDto(
+				authBo.aunthenticateUsuario(authMapper.toUsuario(authLoginRequestDto)));
 		
-		SecurityContextHolder.getContext().setAuthentication(authentication);
-		
-		String token = jwtProvider.generateToken(authentication);
-		
-		return ResponseEntity.status(HttpStatus.OK).body(AuthLoginResponseDto.builder().accessToken(token).build());
+		return ResponseEntity.status(HttpStatus.OK).body(authLoginResponseDto);
 	}
 	
 }
