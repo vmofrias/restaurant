33eef4ddaa65a6dde2002af1138c8c25dea1db6d
Vinicius Frias <vmofrias@gmail.com>
2023-07-03 19:52:59 -0300

    Implementação do método authenticateUsuario()

--- src/main/java/com/ldsk/restaurant/bo/AuthBo.java
+++ src/main/java/com/ldsk/restaurant/bo/AuthBo.java
@@ -1,9 +1,14 @@
 package com.ldsk.restaurant.bo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import com.ldsk.restaurant.model.Usuario;
+import com.ldsk.restaurant.security.JWTUtil;
 import com.ldsk.restaurant.service.RoleService;
 import com.ldsk.restaurant.service.UsuarioService;
 
@@ -18,11 +23,27 @@ public class AuthBo {
 	@Autowired
 	private RoleService roleService;
 	
+	@Autowired
+	private JWTUtil jwtUtil;
+	
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	
 	public Usuario registerUsuario(Usuario usuario) {
 		
 		usuario.setRole(roleService.getRoleByName(REGISTER_DEFAULT_ROLE));
 		
 		return usuarioService.addUsuario(usuario);
+	}
+	
+	public String aunthenticateUsuario(Usuario usuario) {
+		
+		Authentication authentication = authenticationManager.authenticate(
+				new UsernamePasswordAuthenticationToken(usuario.getUsername(), usuario.getPassword()));
+		
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		
+		return jwtUtil.generateToken(authentication);
 	}
 	
 }
