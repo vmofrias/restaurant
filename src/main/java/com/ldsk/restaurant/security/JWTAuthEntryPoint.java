be7562c8a602dc3fbad94d8ae2a75d1f77c8aaed
Vinicius Frias <vmofrias@gmail.com>
2023-07-07 18:35:51 -0300

    Implementação de lógica de erro para o token JWT caso o usuário não tenha autorização para o recurso, ou aquele recurso é proibido para o mesmo, necessitando de outra ROLE

--- src/main/java/com/ldsk/restaurant/security/JWTAuthEntryPoint.java
+++ src/main/java/com/ldsk/restaurant/security/JWTAuthEntryPoint.java
@@ -2,6 +2,8 @@ package com.ldsk.restaurant.security;
 
 import java.io.IOException;
 
+import org.flywaydb.core.internal.util.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -12,12 +14,23 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class JWTAuthEntryPoint implements AuthenticationEntryPoint {
+	
+	@Autowired
+	private JWTUtil jwtUtil;
 
 	@Override
 	public void commence(HttpServletRequest request, HttpServletResponse response,
 			AuthenticationException authException) throws IOException, ServletException {
 		
-		response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+		String jwtToken = jwtUtil.getJWTFromRequest(request);
+		
+		if(StringUtils.hasText(jwtToken) && jwtUtil.validateToken(jwtToken)) {
+			
+			response.sendError(HttpServletResponse.SC_FORBIDDEN, authException.getMessage());
+		} else {
+			
+			response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+		}
 	}
 
 }
