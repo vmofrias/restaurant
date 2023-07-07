5cef295dee31e965801caa9da6884514a694952c
Vinicius Frias <vmofrias@gmail.com>
2023-07-07 18:38:51 -0300

    Ajuste das autorizações para os recursos da API

--- src/main/java/com/ldsk/restaurant/security/SecurityConfig.java
+++ src/main/java/com/ldsk/restaurant/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.ldsk.restaurant.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -18,29 +19,36 @@ import com.ldsk.restaurant.security.filter.JWTAuthenticationFilter;
 @EnableWebSecurity
 public class SecurityConfig {
 	
+	private static final String[] AUTH_WHITELIST = {
+			"/v3/api-docs/**",
+	        "/swagger-ui/**",
+	        "/swagger-ui/",
+	        "/swagger-ui"};
+	
+	private static final String USER_ROLE = "USER";
+	private static final String ADMIN_ROLE = "ADMIN";
+	
 	@Bean
 	public SecurityFilterChain customSecurityFilterChain(HttpSecurity http, JWTAuthEntryPoint jwtAuthEntryPoint) throws Exception {
 		
 		http
 			.csrf().disable()
-			.exceptionHandling()
-			.authenticationEntryPoint(jwtAuthEntryPoint)
+			.exceptionHandling().authenticationEntryPoint(jwtAuthEntryPoint)
 			.and()
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 			.and()
 			.authorizeHttpRequests()
+				.requestMatchers(AUTH_WHITELIST).permitAll()
 				.requestMatchers("/auth/**").permitAll()
-				.anyRequest().hasAnyRole("ADMIN", "USER")
+				.requestMatchers("/cozinha", "/cozinha/**").hasRole(ADMIN_ROLE)
+				.requestMatchers(HttpMethod.GET, "/restaurante").hasAnyRole(ADMIN_ROLE, USER_ROLE)
+				.requestMatchers(HttpMethod.POST, "/restaurante/nome").hasAnyRole(ADMIN_ROLE, USER_ROLE)
+				.requestMatchers("/restaurante", "/restaurante/**").hasRole(ADMIN_ROLE)
+				.anyRequest().authenticated()
 			.and()
 			.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 				
 		return http.build();
-	}
-	
-	@Bean
-	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-		
-		return authenticationConfiguration.getAuthenticationManager();
 	}
 	
 	@Bean
@@ -53,6 +61,12 @@ public class SecurityConfig {
 	public PasswordEncoder passwordEncoder() {
 		
 		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+		
+		return authenticationConfiguration.getAuthenticationManager();
 	}
 	
 }
