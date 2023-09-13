e03a64b2c0f5c6101c1ebcf1bbd421e8ea026ee8
Vinicius Frias <vmofrias@gmail.com>
2023-09-13 09:02:45 -0300

    Tratamento para quando username ou password forem incorretas

--- src/main/java/com/ldsk/restaurant/bo/AuthBo.java
+++ src/main/java/com/ldsk/restaurant/bo/AuthBo.java
@@ -1,12 +1,14 @@
 package com.ldsk.restaurant.bo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import com.ldsk.restaurant.exception.AuthException;
 import com.ldsk.restaurant.model.Usuario;
 import com.ldsk.restaurant.security.JWTUtil;
 import com.ldsk.restaurant.service.RoleService;
@@ -16,6 +18,7 @@ import com.ldsk.restaurant.service.UsuarioService;
 public class AuthBo {
 	
 	private static final String REGISTER_DEFAULT_ROLE = "USER";
+	private static final String ERRO_REQUISICAO = "Erro na requisição";
 
 	@Autowired
 	private UsuarioService usuarioService;
@@ -38,8 +41,16 @@ public class AuthBo {
 	
 	public String aunthenticateUsuario(Usuario usuario) {
 		
-		Authentication authentication = authenticationManager.authenticate(
-				new UsernamePasswordAuthenticationToken(usuario.getUsername(), usuario.getPassword()));
+		Authentication authentication = null;
+		
+		try {
+			
+			authentication = authenticationManager.authenticate(
+					new UsernamePasswordAuthenticationToken(usuario.getUsername(), usuario.getPassword()));
+		} catch (Exception e) {
+
+			throw new AuthException("Username ou password incorreto.", ERRO_REQUISICAO, HttpStatus.BAD_REQUEST);
+		}
 		
 		SecurityContextHolder.getContext().setAuthentication(authentication);
 		
